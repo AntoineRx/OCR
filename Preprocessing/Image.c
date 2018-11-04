e5bcebeed01af7e808738811b303700a2c422dd2
AntoineRx <42241172+AntoineRx@users.noreply.github.com>
2018-11-04 14:23:11 +0100

    Add files via upload

---
+++ Preprocessing/Image.c
@@ -0,0 +1,62 @@
+struct Image {
+	int width;
+	int hieght;
+	int pixels[][];
+};
+
+struct Image *img = malloc(sizeof(*img) + sizeof(int[height][width]));
+
+
+
+Image* Image_create (size_t height, size_t width)
+{
+  // using calloc since it conveniently fills everything with zeroes
+  Image* img = calloc(1, sizeof *img + sizeof(double[height][width]) );
+  img->height = height;
+  img->width = width;
+  // ...
+  return img;
+}
+
+void Image_destroy (Image* img)
+{
+  free(img);
+}
+
+void Image_fill (Image* img)
+{
+  double (*array_2D)[img->width] = (double(*)[img->width]) img->values;
+
+  for(size_t height=0; height < img->height; height++)
+  {
+    for(size_t width=0; width < img->width; width++)
+    {
+      array_2D[height][width] = (double)width; // whatever value that makes sense
+    }
+  }
+}
+
+void Image_print (const Image* img)
+{
+  double (*array_2D)[img->width] = (double(*)[img->width]) img->values;
+
+  for(size_t height=0; height < img->height; height++)
+  {
+    for(size_t width=0; width < img->width; width++)
+    {
+      printf("%.5f ", array_2D[height][width]); 
+    }
+    printf("\n");
+  }
+}
+
+int main (void)
+{
+  int h = 3;
+  int w = 4;
+
+  Image* Image = Image_create(h, w);
+  Image_fill(Image); // fill with some garbage value
+  Image_print(Image);
+  Image_destroy(Image);
+}
